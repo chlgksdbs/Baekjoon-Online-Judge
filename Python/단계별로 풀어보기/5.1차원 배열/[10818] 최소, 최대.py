5d1bfe4390ae837321cf6e0b4b2eeb28bfc11b26
chlgksdbs <96401350+chlgksdbs@users.noreply.github.com>
2022-03-14 22:40:11 +0900

    Create [10818] 최소, 최대.py

---
+++ Python/단계별로 풀어보기/5.1차원 배열/[10818] 최소, 최대.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+arr_max = arr[0]
+arr_min = arr[0]
+for i in range(n):
+    if(arr_max < arr[i]):
+        arr_max = arr[i]
+    if(arr_min > arr[i]):
+        arr_min = arr[i]
+print(arr_min, arr_max)
+
+# 파이썬에서는 n만큼 크기의 입력을 정해두고 받지않고, 입력을 받은 뒤 반복문에서 n만큼 반복하여 마치 n만큼 입력받은 것 같은 효과를 낸다.
