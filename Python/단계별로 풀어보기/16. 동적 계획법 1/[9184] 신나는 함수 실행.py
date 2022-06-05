5a996ca83c88c8bcea2be3a8652d57c5b5eebb12
최한윤 <chlgksdbs98@naver.com>
2022-06-05 21:23:33 +0900

    Create [9184] 신나는 함수 실행.py

---
+++ Python/단계별로 풀어보기/16. 동적 계획법 1/[9184] 신나는 함수 실행.py
@@ -0,0 +1,17 @@
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    elif a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    elif a < b and b < c:
+        return w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+    else:
+        return w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+
+while True:
+    x, y, z = map(int, input().split())
+    if x == -1 and y == -1 and z == -1: # 종료조건
+        break
+    d = [[0, 0, 0] for _ in range(max(x, y, z))] # dp 테이블 생성
+    print(d)
+    print("w(", x, ", ", y, ", ", z, ") = ", w(x, y, z), sep='')
